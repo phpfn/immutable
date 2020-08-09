038f92bc4d884e637850c061640b7a5748a606c8
SerafimArts <nesk@xakep.ru>
2020-08-09 04:53:32 +0300

    Add immutable helper

---
+++ src/Exception/ContextException.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of phpfn package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fun\Immutable\Exception;
+
+/**
+ * An error occurs if it is not possible to obtain a
+ * context for performing a closure or lambda fn.
+ */
+class ContextException extends IntegrityException
+{
+
+}
